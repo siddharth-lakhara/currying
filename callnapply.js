2a0b86e9dd902b17dd8f809c2de2218a4927fe3e
lakhara <siddharth.lakhara@gmail.com>
2018-01-25 21:59:33 +0530

    5th exercise done

---
+++ callnapply.js
@@ -0,0 +1,9 @@
+let callAndApply = {
+	caller: function (object, method, nameArg, ageArg, tShirtSizeArg) {
+		method.call(object, nameArg, ageArg, tShirtSizeArg);
+	},
+	applier: function (object, method, argumentsArr) {
+		method.apply(object, argumentsArr);
+	}
+};
+module.exports = callAndApply;
